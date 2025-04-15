10325d1654daa2c4e5ec2c32e94fcec25ba1a8bc
Sabrinasilva335 <sabrina.squeiroz2@senacsp.edu.br>
2025-04-15 16:03:50 -0300

    Tela de Perfil

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,42 @@ namespace Reformix
         public Form1()
         {
             InitializeComponent();
+           
+            btnAlterarFoto.Click += btnAlterarFoto_Click;
+            btnEditarPerfil.Click += btnEditarPerfil_Click;
+
         }
+
+        private void btnAlterarFoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Selecionar uma foto";
+            ofd.Filter = "Imagens|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                picFotoPerfil.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+        private void btnEditarPerfil_Click(object sender, EventArgs e)
+        {
+            bool emVisualizacao = txtNome.ReadOnly;
+
+            txtNome.ReadOnly = !emVisualizacao;
+            txtEmail.ReadOnly = !emVisualizacao;
+            mtbTelefone.ReadOnly = !emVisualizacao;
+
+            if (emVisualizacao)
+            {
+                btnEditarPerfil.Text = "Salvar Perfil";
+            }
+            else
+            {
+                btnEditarPerfil.Text = "Editar Perfil";
+                MessageBox.Show("Perfil salvo com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
     }
 }
