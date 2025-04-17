4013c52828f1c6dd8032a3a5f82375fb5dc88bd2
AlineConchetta <alineconchetta@gmail.com>
2025-04-17 16:13:14 -0300

    Remoção de alterações dentro do Reformix.csproj. Necessário corrigir o Form1.Designer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -49,3 +49,4 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing
+              
