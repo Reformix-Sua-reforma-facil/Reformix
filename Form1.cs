ee5709ad36f4562093b8221b31f6b9bb6ff1678d
aline.dorini <alineconchetta@gmail.com>
2025-04-17 02:09:44 -0300

    Revert "Correção nome do arquivo: de Form1 para TelaPerfil"
    This reverts commit d253ae4ddcdc3e6462fd7883754142894e4483bc.

---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reformix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string email = txtEmail.Text;
+            string senha = txtSenha.Text;
+            string confirmaçãosenha = txtSenha.Text;
+
+            if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Aqui você pode salvar em banco de dados, arquivo, ou apenas exibir.
+            MessageBox.Show($"Usuário {nome} cadas
