34c0a67f4ee61775596e30aefed9b437968b9e2f
AlineConchetta <alineconchetta@gmail.com>
2025-04-17 16:26:22 -0300

    correção form1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,47 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnEditarPerfil = new System.Windows.Forms.Button();
-            this.grpInfoPerfil = new System.Windows.Forms.GroupBox();
-            this.mtbTelefone = new System.Windows.Forms.MaskedTextBox();
-            this.lblTelefone = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.lblEmail = new System.Windows.Forms.Label();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.lblNome = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnAlterarFoto = new System.Windows.Forms.Button();
-            this.picFotoPerfil = new System.Windows.Forms.PictureBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.lblLogoTexto = new System.Windows.Forms.Label();
-            this.picLogo = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
-            this.grpInfoPerfil.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picFotoPerfil)).BeginInit();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // Form1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.White;
-            this.groupBox1.Controls.Add(this.btnEditarPerfil);
-            this.groupBox1.Controls.Add(this.grpInfoPerfil);
-            this.groupBox1.Controls.Add(this.btnAlterarFoto);
-            this.groupBox1.Controls.Add(this.picFotoPerfil);
-            this.groupBox1.Controls.Add(this.groupBox2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(435, 616);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            // 
-            // btnEditarPerfil
-            // 
-            this.btnEditarPerfil.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditarPerfil.Location = new System.Drawing.Point(153, 563);
-            this.btnEditarPerfil.Name = "btnEditarPerfil";
-            this.btnEditarPerfil.Size = new System.Drawing.Size(116, 36);
-            this.btnEditarPerfil.TabIndex = 5
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
